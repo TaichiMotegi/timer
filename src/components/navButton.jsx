ce7e2739aea885b02b913ba631359b0a77b13dd9
Taichi Motegi <motegi.web@gmail.com>
2024-04-01 11:51:16 +0900

    fix：navigationボタンのリンク先に飛べる範囲をボタンいっぱいに修正

--- src/components/navButton.jsx
+++ src/components/navButton.jsx
@@ -3,27 +3,35 @@ import { Link } from "react-router-dom";
 export const NavButton = (props) => {
   return (
     <div className="flex justify-end fixed top-12 left-15">
-      <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500">
-        <Link to="/">HOME</Link>
-      </button>
-      <button
-        className={
-          props.flag
-            ? "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
-            : "pointer-events-none text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
-        }
-      >
-        <Link to="/timer">TIMER</Link>
-      </button>
-      <button
-        className={
-          props.flag
-            ? "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
-            : "pointer-events-none text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
-        }
-      >
-        <Link to="/result">RESULT</Link>
-      </button>
+      <Link to="/">
+        <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500">
+          HOME
+        </button>
+      </Link>
+
+      <Link to="/timer">
+        <button
+          className={
+            props.flag
+              ? "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
+              : "pointer-events-none text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
+          }
+        >
+          TIMER
+        </button>
+      </Link>
+
+      <Link to="/result">
+        <button
+          className={
+            props.flag
+              ? "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
+              : "pointer-events-none text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
+          }
+        >
+          RESULT
+        </button>
+      </Link>
     </div>
   );
 };
